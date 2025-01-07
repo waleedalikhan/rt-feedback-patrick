516c5a6537b4b448455c415fb4a135c7f15e37a4
waleedalikhan <waleedomore@gmail.com>
2025-01-07 23:12:38 +0500

    updates on edit reply feature

--- src/global/text-replies.js
+++ src/global/text-replies.js
@@ -1,16 +1,24 @@
 export const textReplies = (data) => ({
   reply: data,
+  replyToEdit: data.replyComment,
   isEditReply: false,
   isEditReplyLoading: false,
   isRemoveReplyLoading: false,
   // Function for editing reply
-  editReply() {
+  editReply(index) {
     this.isEditReplyLoading = true;
     setTimeout(() => {
+      this.reply.replyComment = this.replyToEdit;
+      console.log(this.reply);
+      this.$el.dispatchEvent(
+        new CustomEvent("edit-reply", {
+          detail: { index, reply: this.reply },
+          bubbles: true,
+        })
+      );
       this.isEditReplyLoading = false;
       this.isEditReply = false;
       // Updated reply object
-      console.log(this.reply);
       // Api call to edit reply here!
     }, 2000);
   },
